209bfe95beefbb5c772085a356b61b9978ffded0
Marcos Pimentel <106275499+dopimentel@users.noreply.github.com>
2025-06-15 16:49:03 -0300

    chore: updat 01_django_commands

--- .platform/hooks/postdeploy/01_django_commands.sh
+++ .platform/hooks/postdeploy/01_django_commands.sh
@@ -6,6 +6,7 @@ set -e
 # ADICIONA O DIRETÓRIO DO POETRY AO PATH DO SISTEMA
 export PATH=$PATH:/root/.local/bin
 
+
 # O Elastic Beanstalk exporta as variáveis de ambiente para um arquivo.
 # Carregar este arquivo garante que todas as variáveis (DB_HOST, etc.)
 # estejam disponíveis para os comandos seguintes.
